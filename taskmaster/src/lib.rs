9dac0e813af3d6cdb316ea5fd3f7237754a38c03
Bourret Lucas <bourretlucas@gmail.com>
2023-02-08 20:24:51 +0100

    command_builder

--- taskmaster/src/lib.rs
+++ taskmaster/src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs;
-use std::process::{Command, Child};
+use std::fs::File;
+use std::process::{Command, Child, Stdio};
 use serde::Deserialize;
 use std::error::Error;
 use std::collections::HashMap;
@@ -15,21 +16,23 @@ struct Processus {
     id: usize,
     name: String,
     child: Option<Child>,
+    retries: usize,
 }
 
 impl Processus {
-    fn build(id: usize, name: &str) -> Self {
+    fn build(id: usize, name: &str, retries: usize) -> Self {
         Self {
             id,
             name: name.to_owned(),
             child: None,
+            retries,
         }
     }
 }
 
 #[derive(Deserialize, Debug, Default)]
 #[serde(deny_unknown_fields, default)]
-struct Taks {
+struct Task {
     cmd: String,
     numprocs: usize,
     umask: String,
@@ -53,7 +56,7 @@ pub struct Taskmaster {
     #[serde(skip)]
     procs: Vec<Processus>,
     #[serde(flatten)]
-    config: HashMap<String, Taks>,
+    config: HashMap<String, Task>,
 }
 
 impl Taskmaster {
@@ -61,7 +64,7 @@ impl Taskmaster {
     pub fn build(file_path: &str) -> Result<Self, Box<dyn Error>> {
         let commands: Vec<Processus> = vec!();
         let config = fs::read_to_string(file_path)?;
-        let config: HashMap<String, Taks> = serde_yaml::from_str(&config)?;
+        let config: HashMap<String, Task> = serde_yaml::from_str(&config)?;
 
         Ok(Taskmaster {
             procs: commands,
@@ -69,23 +72,40 @@ impl Taskmaster {
         })
     }
 
-    pub fn execute(& mut self) {
+    pub fn execute(& mut self) -> Option<Box<dyn Error>> {
         for (name, properties) in self.config.iter_mut() {
-            let args: Vec<&str> = properties.cmd.split_ascii_whitespace().collect();
 
-            //TODO fn build_command() -> Option<Command>
-            properties.command = Some(Command::new(properties.cmd.split_once(" ").unwrap().0));
+            match Self::build_command(properties) {
+                Some(error) => return Some(error),
+                None => {},
+            }
+
             for id in 0..properties.numprocs {
-                self.procs.push(Processus::build(id, name));
+                self.procs.push(Processus::build(id, name, properties.startretries));
             }
         }
+        None
     }
 
-    fn build_command(task: Taks) -> Option<Command> {
+    fn build_command(task: &mut Task) -> Option<Box<dyn Error>> {
         let cmd: Vec<&str> = task.cmd.split_whitespace().collect();
-        
+        let output = Self::creat_output(&task).unwrap();
         task.command = Some(Command::new(cmd.get(0).unwrap()));
 
-        todo!()
+        task.command.as_mut().unwrap().args(cmd.get(1).iter())
+            .envs(task.env.iter())
+            .current_dir(&task.workingdir)
+            .stdout(output.0)
+            .stderr(output.1);
+        
+        None
     }
+
+    fn creat_output(task: &Task) -> Result<(Stdio, Stdio), Box<dyn Error>> {
+        let stdout = Stdio::from(File::open(&task.stdout).unwrap_or(File::create(&task.stdout)?));
+        let stderr = Stdio::from(File::open(&task.stderr).unwrap_or(File::create(&task.stderr)?));
+
+        Ok((stdout, stderr))
+    }
+
 }
